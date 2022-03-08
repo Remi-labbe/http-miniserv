bcfb020b34f9cb510257ebe1e01e09da4f721790
Remi Labbe <labbe.remi@free.fr>
2022-03-08 16:29:57 +0100

    serve html files + add default files

--- server.c
+++ server.c
@@ -10,25 +10,32 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
+// #include <regex.h>
 
 #include "internetaddr/adresse_internet.h"
 #include "internetaddr/adresse_internet_type.h"
 #include "tcpsockets/tcp_socket.h"
 #include "tcpsockets/tcp_socket_type.h"
 
-#define BUF_SIZE 512
+#define BUF_SIZE 1024
 
 #define STATUS_OK 200
 #define STATUS_NOT_MODIFIED 304
 #define STATUS_BAD_REQUEST 400
+#define STATUS_FORBIDDEN 403
 #define STATUS_NOT_FOUND 404
+#define STATUS_INTERNAL_SERVER_ERROR 500
 #define STATUS_NOT_IMPLEMENTED 501
+
+#define TYPE_GET "GET"
 
 #define SERVER_NAME "http_miniserv"
 #define PORT 8080
 
 tcp_socket *local;
 adresse_internet *addr;
+
+int header(tcp_socket *client, int scode, off_t fsize);
 
 // Threads related
 /**
@@ -123,88 +130,184 @@ void start_th(tcp_socket *c) {
   int r;
   if ((r = pthread_attr_init(&attr)) != 0) {
     fprintf(stderr, "pthread_attr_init: %s\n", strerror(r));
+    close_socket_tcp(c);
     return;
   }
   if ((r = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)) != 0) {
     fprintf(stderr, "pthread_attr_setdetachstate: %s\n", strerror(r));
+    close_socket_tcp(c);
     return;
   }
   if ((r = pthread_create(&th, &attr, (void *(*)(void *))th_routine, c)) != 0) {
     fprintf(stderr, "pthread_create: %s\n", strerror(r));
+    close_socket_tcp(c);
     return;
   }
 }
 
 void *th_routine(tcp_socket *client) {
-  char req[128] = {0};
+  char req[BUF_SIZE] = {0};
   printf("\n---------\n");
-  recv(client->fd, req, 128, 0);
+  recv(client->fd, req, BUF_SIZE, 0);
   printf("%s", req);
-  printf("\n---------\n");
-  int status = STATUS_OK;
-  char buf[BUF_SIZE] = {0};
-  char now[128] = {0};
-  time_t t = time(NULL);
-  struct tm *tm = localtime(&t);
-  if (tm == NULL) {
+  printf("---------\n");
+  //
+  // TODO: Check request format
+  //
+  char *method, *path, *saveptr;
+  method = strtok_r(req, " ", &saveptr);
+  if (strcmp(method, TYPE_GET) != 0) {
+    header(client, STATUS_NOT_IMPLEMENTED, 0);
     close_socket_tcp(client);
     return NULL;
   }
-  strftime(now, 128, "%a, %d %b %Y %H:%M:%S GMT", tm);
-  if (status == STATUS_OK) {
-    sprintf(buf,
-            "HTTP/1.1 200 OK\r\n"
-            "Date: %s\r\n"
-            "Server: " SERVER_NAME "\r\n"
-            "Content-type: text/html\r\n"
-            // "Content-length: %zu\r\n"
-            // "\r\n",
-            ,
-            now);
+  path = strtok_r(NULL, " ", &saveptr);
+  if (path[0] == '/') {
+    ++path;
   }
-  if (write_socket_tcp(client, buf, strlen(buf)) == -1) {
-    fprintf(stderr, "Couldn't send response\n");
+  if (access(path, F_OK) != 0) {
+    header(client, STATUS_NOT_FOUND, 0);
+    close_socket_tcp(client);
+    return NULL;
+  } else if (access(path, R_OK) != 0) {
+    header(client, STATUS_FORBIDDEN, 0);
     close_socket_tcp(client);
     return NULL;
   }
-  int file = open("index.html", O_RDONLY, S_IRUSR);
+  //
+  int file = open(path, O_RDONLY, S_IRUSR);
   if (file == -1) {
-    fprintf(stderr, "Couldn't open file\n");
+    header(client, STATUS_INTERNAL_SERVER_ERROR, 0);
     close_socket_tcp(client);
     return NULL;
   }
   struct stat s;
-  if (fstat(client->fd, &s) == -1) {
-    fprintf(stderr, "Error on file\n");
-    close_socket_tcp(client);
-    return NULL;
-  }
-  ssize_t blk_client = s.st_blksize;
   if (fstat(file, &s) == -1) {
-    fprintf(stderr, "Error on file\n");
+    header(client, STATUS_INTERNAL_SERVER_ERROR, 0);
     close_socket_tcp(client);
     return NULL;
   }
-  memset(buf, 0, BUF_SIZE);
-  sprintf(buf,
-          "Content-length: %zu\r\n"
-          "\r\n",
-          s.st_size);
-  if (write_socket_tcp(client, buf, strlen(buf)) == -1) {
-    fprintf(stderr, "Couldn't send response\n");
+  if ((s.st_mode & S_IFMT) != S_IFREG) {
+    header(client, STATUS_BAD_REQUEST, 0);
     close_socket_tcp(client);
     return NULL;
   }
-  ssize_t size = s.st_blksize > blk_client ? blk_client : s.st_blksize;
+  off_t fsize = s.st_size;
+  ssize_t fblk = s.st_blksize;
+  if (fstat(client->fd, &s) == -1) {
+    header(client, STATUS_INTERNAL_SERVER_ERROR, 0);
+    close_socket_tcp(client);
+    return NULL;
+  }
+  header(client, STATUS_OK, fsize);
+  ssize_t size = s.st_blksize > fblk ? fblk : s.st_blksize;
+  // printf("fsize: %zu\nfblk: %zu\nsize: %zu\n", fsize, fblk, size);
   char msg[size];
   memset(msg, 0, (size_t)size);
   while (read(file, msg, (size_t)size) > 0) {
     if (write_socket_tcp(client, msg, strlen(msg)) == -1) {
-      fprintf(stderr, "Couldn't send response\n");
-      close_socket_tcp(client);
-      return NULL;
+      break;
     }
+    printf(">WRITE\n");
+    memset(msg, 0, (size_t)size);
   }
+  printf(">END\n");
   close_socket_tcp(client);
   return NULL;
+}
+
+// int redFormat(char *s) {
+//   regex_t reg;
+//   regcomp(&reg, "", REG_EXTENDED);
+//   return 0;
+// }
+
+int status(int code, char *buf) {
+  switch (code) {
+  case STATUS_OK:
+    strcpy(buf, "Ok");
+    return 0;
+  // case STATUS_NOT_MODIFIED:
+  //   strcpy(buf, "Not Modified");
+  //   return 0;
+  case STATUS_BAD_REQUEST:
+    strcpy(buf, "Bad Request");
+    return 0;
+  case STATUS_FORBIDDEN:
+    strcpy(buf, "Forbidden");
+    return 0;
+  case STATUS_NOT_FOUND:
+    strcpy(buf, "Not Found");
+    return 0;
+  case STATUS_INTERNAL_SERVER_ERROR:
+    strcpy(buf, "Internal Server Error");
+    return 0;
+  case STATUS_NOT_IMPLEMENTED:
+    strcpy(buf, "Not Implemented");
+    return 0;
+  default:
+    strcpy(buf, "Internal Server Error");
+    return -1;
+  }
+}
+
+int header(tcp_socket *client, int scode, off_t fsize) {
+  char buf[BUF_SIZE] = {0};
+  char status_str[128] = {0};
+  if (status(scode, status_str) == -1) {
+    return -1;
+  }
+  char now[128] = {0};
+  time_t t = time(NULL);
+  struct tm *tm = localtime(&t);
+  if (tm == NULL) {
+    return -1;
+  }
+  strftime(now, 128, "%a, %d %b %Y %H:%M:%S GMT", tm);
+  switch (scode) {
+  case STATUS_OK:
+    sprintf(buf,
+            "HTTP/1.0 %d %s\r\n"
+            "Date: %s\r\n"
+            "Server: " SERVER_NAME "\r\n"
+            "Content-type: text/html\r\n"
+            "Content-length: %zu\r\n"
+            "\r\n",
+            scode, status_str, now, fsize);
+    break;
+  // case STATUS_NOT_MODIFIED:
+  //   sprintf(buf,
+  //           "HTTP/1.0 %d %s\r\n"
+  //           "Date: %s\r\n"
+  //           "Server: " SERVER_NAME "\r\n"
+  //           "Content-type: text/html\r\n",
+  //           scode, status_str, now);
+  //   break;
+  case STATUS_BAD_REQUEST:
+  case STATUS_FORBIDDEN:
+  case STATUS_NOT_FOUND:
+  case STATUS_INTERNAL_SERVER_ERROR:
+  case STATUS_NOT_IMPLEMENTED:
+    sprintf(buf,
+            "HTTP/1.0 %d %s\r\n"
+            "Date: %s\r\n"
+            "Server: " SERVER_NAME "\r\n"
+            "Content-type: text/html\r\n"
+            "Connection: close\r\n",
+            scode, status_str, now);
+    break;
+  default:
+    sprintf(buf,
+            "HTTP/1.0 %d %s\r\n"
+            "Date: %s\r\n"
+            "Server: " SERVER_NAME "\r\n"
+            "Content-type: text/html\r\n"
+            "Connection: close\r\n",
+            STATUS_INTERNAL_SERVER_ERROR, status_str, now);
+  }
+  if (write_socket_tcp(client, buf, strlen(buf)) == -1) {
+    fprintf(stderr, "Couldn't send response\n");
+    return -1;
+  }
+  return 0;
 }
